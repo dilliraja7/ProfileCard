9ce5fd8c4a742aa65b341762532df5e2f205d74b
dilliraja7 <146093183+dilliraja7@users.noreply.github.com>
2025-08-24 22:26:47 +0530

    first commit

--- src/Component/ProfileCard/ProfileCard.jsx
+++ src/Component/ProfileCard/ProfileCard.jsx
@@ -8,7 +8,7 @@ const ProfileCard = () =>{
         <h2>ProfileCard</h2>
         <div className='card'>
             
-            <img src="/src/assets/DILLIRAJA L.jpg" alt="Profile Avatar" />
+            <img src="DILLIRAJA L.jpg" alt="Profile Avatar" />
             <Name firstName={'DILLIRAJA'} lastName={'L'}></Name>
             <h2>Age-22</h2>
             <h3>Location Tiruttani</h3>
